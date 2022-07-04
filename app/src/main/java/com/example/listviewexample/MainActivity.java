8f433cc5d63161983cdac889b60d884372bd6228
Mohammed <mhmdtajeldin@gmail.com>
2022-07-04 09:10:46 +0200

    Add all code for listview

---
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String tutorials[]
+            = { "Algorithms", "Data Structures",
+            "Languages", "Interview Corner",
+            "GATE", "ISRO CS",
+            "UGC NET CS", "CS Subjects",
+            "Web Technologies" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        l = findViewById(R.id.list);
+        ArrayAdapter<String> arr;
+        arr
+                = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                tutorials);
+        l.setAdapter(arr);
+    }
+}
