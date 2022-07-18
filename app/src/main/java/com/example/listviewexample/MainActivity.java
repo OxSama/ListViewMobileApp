b36bfb13b81b8bcdc6c32a95e7a2f23e37d3aa3d
Mohammed <mhmdtajeldin@gmail.com>
2022-07-18 08:32:52 +0200

    In lab edits

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         l = findViewById(R.id.list);
         ArrayAdapter<String> arr;
         arr
